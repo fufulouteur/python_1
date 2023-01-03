aa58d64ae893c9e4882670d793017c4eb8f624ce
fufulouteur <manzodqb@icloud.com>
2023-01-03 16:12:20 -0500

    premier commit fini page 35

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+
+
+
+
+
+
+# titre_film = 'toy story 4'
+# sortie = 2019
+# score = '4/9'
+# descriptif = 'rhfkjshfhfkjsdhfksdhfdsh shdfushd f sodhhdsfkds sudhfusd fu'
+#
+# print('titre de filme =' ,titre_film)
+# print(sortie)
+# print(score)
+# print(descriptif)
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #depart#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
